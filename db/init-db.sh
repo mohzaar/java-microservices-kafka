003b720d2d175c58bfbd6ba90816607be0760f91
mohzaar <lazaar.75017@gmail.com>
2020-12-12 21:26:59 +0100

    :wrench: docker-compose & application.properties

--- db/init-db.sh
+++ db/init-db.sh
@@ -3,7 +3,7 @@ set -e
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
     CREATE DATABASE ordering-db;
-    GRANT ALL PRIVILEGES ON DATABASE ordering-db TO dbuser;
+    GRANT ALL PRIVILEGES ON DATABASE ordering-db TO dev;
     CREATE DATABASE shipping-db;
-    GRANT ALL PRIVILEGES ON DATABASE shipping-db TO dbuser;
+    GRANT ALL PRIVILEGES ON DATABASE shipping-db TO dev;
 EOSQL
